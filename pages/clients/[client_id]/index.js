ff7eb55a360666c17ec0a78e9227f32c69d80282
jmarcossf <jamlmarcos@gmail.com>
2022-12-11 22:05:49 -0800

    Programmatic navigation examples.

--- pages/clients/[client_id]/index.js
+++ pages/clients/[client_id]/index.js
@@ -1,12 +1,24 @@
 import { useRouter } from "next/router";
 
+/** Programmatic navigation */
 function ClientProjectPage() {
-  const { query } = useRouter();
+  const { query, push, replace } = useRouter();
+
+  const handleProjectButtonClick = () => {
+    // do stuff here...
+    push({
+      pathname: "/clients/[client_id]/[client_project_id]",
+      query: { client_id: "abc", client_project_id: "projectX" },
+    });
+    // push("/clients/abc/projectA");
+    // replace("/clients/abc/projectA");
+  };
 
   return (
     <div>
       <h1>A Client's Project</h1>
       {query.client_id ? <div>Client id: {query.client_id}</div> : null}
+      <button onClick={handleProjectButtonClick}>Go To Client Projects</button>
     </div>
   );
 }
