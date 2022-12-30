c05ba014ab0209e8fdf3602141c3da5125c86bdb
jmarcossf <jamlmarcos@gmail.com>
2022-12-29 20:20:46 -0800

    First usage of getStaticProps();.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
-import styles from '../styles/Home.module.css'
+// import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home(props) {
+  const { products } = props;
   return (
-    <div className={styles.container}>
-      <h1>Hello Next World!</h1>
-    </div>
-  )
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+// getStaticProps get's executed FIRST before the component function
+// The below ALL happens on the server side IN ADVANCE
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [
+        { id: 1, title: "Product 1" },
+        { id: 2, title: "Product 2" },
+        { id: 3, title: "Product 3" },
+      ],
+    },
+  };
 }
