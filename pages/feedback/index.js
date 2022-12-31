78d0301cdf6fcdc12e6c5f6c0a3203e989d1baa1
jmarcossf <jamlmarcos@gmail.com>
2022-12-31 01:11:10 -0800

    Added a path for to view individual feedback. Also added the necessary API routes to get the feedback ID.

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -5,8 +5,8 @@ import { buildFeedbackPath, getFeedbackData } from '../api/feedback/index';
 function FeedbackPage(props) {
   const [feedbackData, setFeedbackData] = useState();
 
-  function loadFeedbackHandler(id) {
-    fetch(`/api/feedback/${id}`)
+  function loadFeedbackHandler(feedbackId) {
+    fetch(`/api/feedback/${feedbackId}`)
       .then((response) => response.json())
       .then((data) => {
         setFeedbackData(data.feedback);
