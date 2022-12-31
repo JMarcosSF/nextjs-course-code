75fd90d7bdad7d5f7cf1074381ac32204ddffff6
jmarcossf <jamlmarcos@gmail.com>
2022-12-30 22:28:35 -0800

    Added dev data in feedback.json. Updated api feedback.js with handler to get, update and return the data from the file system. Extracted the feedback form into FeedbackForm.jsx.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 import FeedbackForm from "../components/FeedbackForm";
 
 function HomePage() {
-
-
   return (
     <div>
       <h1>The Home Page</h1>
