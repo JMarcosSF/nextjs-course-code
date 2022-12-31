d58343a9842346b11124ab2f68cf1336bf160f43
jmarcossf <jamlmarcos@gmail.com>
2022-12-31 01:15:33 -0800

    Added a path for to view individual feedback. Also added the necessary API routes to get the feedback ID.

--- pages/api/feedback/[feedbackId].js
+++ pages/api/feedback/[feedbackId].js
@@ -1,6 +1,8 @@
 import { buildFeedbackPath, getFeedbackData } from '.';
 
 function handler(req, res) {
+  // Could also handle different request types here with
+  // if (req.method === "POST" or "DELETE" or whatever
   const feedbackId = req.query.feedbackId;
   const filePath = buildFeedbackPath();
   const feedbackData = getFeedbackData(filePath);
